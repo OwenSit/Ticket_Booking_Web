d5a21499ad35c3ac7eb4023f8d14b4fefc7f98af
SongwenXue <60166898+OwenSit@users.noreply.github.com>
2020-11-30 10:42:10 -0600

    Aki's Version Uploaded

---
+++ client/main.js
@@ -0,0 +1,184 @@
+// set global variable todos
+let tickett = []
+
+// function to set todos
+const setReserve = (data) => {
+  tickett = data;
+}
+
+tax = 0.08; //8% tax
+/******************************************************************* */
+
+let flights = [];
+
+// function to set todos
+const setFlights = (data) => {
+  flights = data;
+};
+
+const displayFlights = () => {
+  const todoTable = document.querySelector("#flight-table");
+
+  // display all todos by modifying the HTML in "todo-table"
+  let tableHTML = "";
+  flights.map((flights) => {
+    tableHTML += `<tr key=${flights.todflo_id}>
+      <th>${flights.flight_id}</th>
+      <th>${flights.scheduled_departure}</th>
+      <th>${flights.scheduled_arrival}</th>
+      <th>${flights.departure_airport}</th>
+      <th>${flights.stop1_airport}</th>
+      <th>${flights.stop2_airport}</th>
+      <th>${flights.arrival_airport}</th>
+      <th>${flights.seats_available}</th>
+      <th>${flights.status}</th>
+      
+      </tr>`;
+  });
+  todoTable.innerHTML = tableHTML;
+};
+
+
+// select all the flights info
+async function flightsInfo() {
+  // use try... catch... to catch error
+  try {
+    // GET all todos from "http://localhost:5000/todos"
+    const response = await fetch("http://localhost:5000");
+    const jsonData = await response.json();
+
+    setFlights(jsonData);
+    displayFlights();
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+flightsInfo();
+
+
+
+
+/********************************************************************/
+
+
+
+// function to display todos
+const displayReserve = () => {
+  const todoTable = document.querySelector('#reserve-table');
+
+  // display all todos by modifying the HTML in "todo-table"
+  let tableHTML = "";
+  tickett.map(tickett =>{
+    tableHTML +=
+    `<tr key=${tickett.ticket_no}>
+    <th>${tickett.flight_id}</th>
+    <th>${tickett.name}</th>
+    <th>${tickett.phone}</th>
+    <th>${tickett.email}</th>
+    <th>${tickett.checked_bag}</th>
+    <th>${tickett.movie}</th>
+    <th>${tickett.meal}</th>
+    <th>${tickett.amount_wotax * (1+tax)}</th>
+    
+ 
+    <th><button class="btn btn-danger" type="button" onclick="deleteCustomer(${tickett.ticket_no})">Delete</button></th>
+    </tr>`;
+  })
+  todoTable.innerHTML = tableHTML;
+
+}
+
+// select all the reserve when the codes first run
+selectReserve();
+
+
+// The following are async function to select, insert, update and delete todos
+// select all the todos
+async function selectReserve() {
+  // use try... catch... to catch error
+  try {
+
+    // GET all todos from "http://localhost:5000/todos"
+    const response = await fetch("http://localhost:5000/book")
+    const jsonData = await response.json();
+
+    setReserve(jsonData);
+    displayReserve();
+
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+// insert a new todo
+async function insertCustomer() {
+  // read the todo description from input
+  const fid = document.querySelector('#flightID').value;
+  const name = document.querySelector('#name').value;
+  const pho = document.querySelector('#phone').value;
+  const em = document.querySelector('#email').value;
+  const bg = document.querySelector('#bags').value;
+  const mv = document.querySelector('#movie').value;
+  const ml = document.querySelector('#meal').value;
+  const ag = document.querySelector('#age').value;
+  
+  
+  price = 1000;
+  dicount = "False";
+
+  if(ag < 18 || ag > 56)
+  {
+    dicount = "True";
+    price = price * 0.8; //20% off
+  }
+  
+
+  //flight_id,movie,meal,name,checked_bag,amount_woTax,discount,phone,emal
+  st = fid+','+mv+','+ml+','+name+','+bg+','+price.toString()+','+dicount+','+pho+','+em;
+
+  //alert(st);
+
+  // use try... catch... to catch error
+  try {
+
+    // insert new name to "http://localhost:5000/book", with "POST" method
+    const body = { st: st };
+    const response = await fetch("http://localhost:5000/book", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body)
+    });
+
+    
+  
+
+    // refresh the page when inserted
+    location.reload();
+    return false;
+
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+
+
+// delete a todo by id
+async function deleteCustomer(id) {
+  try {
+    // delete a name from "http://localhost:5000/name/${id}", with "DELETE" method
+    const deleteCustomer = await fetch(`http://localhost:5000/book/${id}`, {
+      method: "DELETE"
+    })
+
+    // refresh the page when deleted
+    location.reload();
+    return false;
+
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+
---
+++ server/server.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const pool = require('./db');
+
+// middleware
+app.use(cors());
+app.use(express.json());      //req.body
+
+//ROUTES
+
+//insert a todo
+app.post('/book', async(req, res)=>{
+  try{
+    
+    const {st} = req.body;
+    console.log(st);
+    info = st.split(',')
+    
+    const newReserve = await pool.query(`INSERT INTO tickett (flight_id, movie, meal, name, checked_bag, amount_woTax, discount, phone, email) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
+      [info[0], info[1], info[2], info[3], info[4], info[5], info[6], info[7], info[8]]);
+
+    res.json(newReserve);              
+
+  } catch(err){
+    console.log(err.message);
+  }
+});
+
+/****************************************************************** */
+// flight info:
+app.get("/", async (req, res) => {
+  try {
+    const flightInfo = await pool.query(`SELECT * FROM flights`);
+    res.json(flightInfo.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+/********************************************************************* */
+//get all todo
+app.get('/book', async(req, res)=>{
+  try{
+    const allReserve = await pool.query(`SELECT * FROM tickett`);
+    res.json(allReserve.rows);
+  } catch(err){
+    console.log(err.message);
+  }
+});
+
+//get a todo by id
+app.get('/book/:id', async(req, res)=>{
+  try{
+    const { id } = req.params;
+    const reserve = await pool.query(`SELECT * FROM tickett 
+                                   WHERE id = $1`, 
+      [id]);
+    res.json(reserve.rows);
+  } catch(err){
+    console.log(err.message);
+  }
+});
+
+//update a todo by id
+app.put("/book/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+    const updateReserve = await pool.query(`UPDATE tickett SET name = $1 
+                                         WHERE id = $2`,
+      [name, id]);
+    res.json("Todo was updated!");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//delete a todo by id
+app.delete("/book/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteReserve = await pool.query(`DELETE FROM tickett 
+                                         WHERE id = $1`, 
+      [id]);
+    res.json("Todo was deleted!");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// set up the server listening at port 5000 (the port number can be changed)
+app.listen(5000, ()=>{
+  console.log("server has started on port 5000");
+});
