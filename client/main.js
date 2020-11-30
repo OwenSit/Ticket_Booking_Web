2d42eb3413ae68ef0d72a8bfa8716c28279d9b01
SongwenXue <60166898+OwenSit@users.noreply.github.com>
2020-11-30 17:32:32 -0600

    Update main.js

--- client/main.js
+++ client/main.js
@@ -1,10 +1,10 @@
 // set global variable todos
-let tickett = []
+let tickett = [];
 
 // function to set todos
 const setReserve = (data) => {
   tickett = data;
-}
+};
 
 tax = 0.08; //8% tax
 /******************************************************************* */
@@ -15,6 +15,11 @@ let flights = [];
 const setFlights = (data) => {
   flights = data;
 };
+
+function parseISOString(s) {
+  var b = s.split(/\D+/);
+  return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+}
 
 const displayFlights = () => {
   const todoTable = document.querySelector("#flight-table");
@@ -38,7 +43,7 @@ const displayFlights = () => {
   todoTable.innerHTML = tableHTML;
 };
 
-
+var x;
 // select all the flights info
 async function flightsInfo() {
   // use try... catch... to catch error
@@ -48,6 +53,12 @@ async function flightsInfo() {
     const jsonData = await response.json();
 
     setFlights(jsonData);
+
+    for (x of jsonData) {
+      x.scheduled_arrival = parseISOString(x.scheduled_arrival);
+      x.scheduled_departure = parseISOString(x.scheduled_departure);
+    }
+    // console.log(parseISOString(jsonData[0].scheduled_arrival));
     displayFlights();
   } catch (err) {
     console.log(err.message);
@@ -56,22 +67,16 @@ async function flightsInfo() {
 
 flightsInfo();
 
-
-
-
 /********************************************************************/
-
-
 
 // function to display todos
 const displayReserve = () => {
-  const todoTable = document.querySelector('#reserve-table');
+  const todoTable = document.querySelector("#reserve-table");
 
   // display all todos by modifying the HTML in "todo-table"
   let tableHTML = "";
-  tickett.map(tickett =>{
-    tableHTML +=
-    `<tr key=${tickett.ticket_no}>
+  tickett.map((tickett) => {
+    tableHTML += `<tr key=${tickett.ticket_no}>
     <th>${tickett.flight_id}</th>
     <th>${tickett.name}</th>
     <th>${tickett.phone}</th>
@@ -79,33 +84,31 @@ const displayReserve = () => {
     <th>${tickett.checked_bag}</th>
     <th>${tickett.movie}</th>
     <th>${tickett.meal}</th>
-    <th>${tickett.amount_wotax * (1+tax)}</th>
+    <th>${tickett.amount_wotax * (1 + tax)}</th>
     
  
-    <th><button class="btn btn-danger" type="button" onclick="deleteCustomer(${tickett.ticket_no})">Delete</button></th>
+    <th><button class="btn btn-danger" type="button" onclick="deleteCustomer(${
+      tickett.ticket_no
+    })">Delete</button></th>
     </tr>`;
-  })
+  });
   todoTable.innerHTML = tableHTML;
-
-}
+};
 
 // select all the reserve when the codes first run
 selectReserve();
-
 
 // The following are async function to select, insert, update and delete todos
 // select all the todos
 async function selectReserve() {
   // use try... catch... to catch error
   try {
-
     // GET all todos from "http://localhost:5000/todos"
-    const response = await fetch("http://localhost:5000/book")
+    const response = await fetch("http://localhost:5000/book");
     const jsonData = await response.json();
 
     setReserve(jsonData);
     displayReserve();
-
   } catch (err) {
     console.log(err.message);
   }
@@ -114,71 +117,75 @@ async function selectReserve() {
 // insert a new todo
 async function insertCustomer() {
   // read the todo description from input
-  const fid = document.querySelector('#flightID').value;
-  const name = document.querySelector('#name').value;
-  const pho = document.querySelector('#phone').value;
-  const em = document.querySelector('#email').value;
-  const bg = document.querySelector('#bags').value;
-  const mv = document.querySelector('#movie').value;
-  const ml = document.querySelector('#meal').value;
-  const ag = document.querySelector('#age').value;
-  
-  
+  const fid = document.querySelector("#flightID").value;
+  const name = document.querySelector("#name").value;
+  const pho = document.querySelector("#phone").value;
+  const em = document.querySelector("#email").value;
+  const bg = document.querySelector("#bags").value;
+  const mv = document.querySelector("#movie").value;
+  const ml = document.querySelector("#meal").value;
+  const ag = document.querySelector("#age").value;
+
   price = 1000;
   dicount = "False";
 
-  if(ag < 18 || ag > 56)
-  {
+  if (ag < 18 || ag > 56) {
     dicount = "True";
     price = price * 0.8; //20% off
   }
-  
 
   //flight_id,movie,meal,name,checked_bag,amount_woTax,discount,phone,emal
-  st = fid+','+mv+','+ml+','+name+','+bg+','+price.toString()+','+dicount+','+pho+','+em;
+  st =
+    fid +
+    "," +
+    mv +
+    "," +
+    ml +
+    "," +
+    name +
+    "," +
+    bg +
+    "," +
+    price.toString() +
+    "," +
+    dicount +
+    "," +
+    pho +
+    "," +
+    em;
 
   //alert(st);
 
   // use try... catch... to catch error
   try {
-
     // insert new name to "http://localhost:5000/book", with "POST" method
     const body = { st: st };
     const response = await fetch("http://localhost:5000/book", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(body)
+      body: JSON.stringify(body),
     });
-
-    
-  
 
     // refresh the page when inserted
     location.reload();
     return false;
-
   } catch (err) {
     console.log(err.message);
   }
 }
-
-
 
 // delete a todo by id
 async function deleteCustomer(id) {
   try {
     // delete a name from "http://localhost:5000/name/${id}", with "DELETE" method
     const deleteCustomer = await fetch(`http://localhost:5000/book/${id}`, {
-      method: "DELETE"
-    })
+      method: "DELETE",
+    });
 
     // refresh the page when deleted
     location.reload();
     return false;
-
   } catch (err) {
     console.log(err.message);
   }
 }
-
-
