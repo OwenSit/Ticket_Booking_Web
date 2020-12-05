0eab846ab32dff2ce2b3a668ede0ffda876ef864
SongwenXue <60166898+OwenSit@users.noreply.github.com>
2020-12-05 01:25:36 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+var crypto = require("crypto");
+
+//function code taken from http://blog.tompawlak.org/how-to-generate-random-values-nodejs-javascript
+function randomValueHex(len) {
+  return crypto
+    .randomBytes(Math.ceil(len / 2))
+    .toString("hex") // convert to hexadecimal format
+    .slice(0, len)
+    .toUpperCase(); // return required number of characters
+}
 
 Date.prototype.addHours = function (h) {
   this.setTime(this.getTime() + h * 60 * 60 * 1000);
@@ -13,64 +23,76 @@ app.use(express.json()); //req.body
 
 //ROUTES
 
-//
 app.post("/book", async (req, res) => {
   try {
-    const { st } = req.body;
-    console.log(st);
-    info = st.split(",");
-
-    if (info[1] == "Y") {
-      info[1] = "t";
-    } else {
-      info[1] = "f";
-    }
-
-    if (info[2] == "Y") {
-      info[2] = "t";
-    } else {
-      info[2] = "f";
-    }
-
-    if (info[6] == "Y") {
-      info[6] = "t";
-    } else {
-      info[6] = "f";
-    }
-
-    const newReserve = await pool.query(
-      `INSERT INTO ticket (flight_id, movie, meal, name, checked_bag, amount_wotax, discount, phone, email) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
-      [
-        info[0],
-        info[1],
-        info[2],
-        info[3],
-        info[4],
-        info[5],
-        info[6],
-        info[7],
-        info[8],
-      ]
+    // STEP ONE: generate a new entry in the bookings table:
+    const book_info = req.body;
+    // console.log(book_info);
+    var book_ref = randomValueHex(6);
+    const exist = await pool.query(
+      "SELECT book_ref FROM bookings where book_ref=$1",
+      [book_ref]
     );
-    // update the seatnumber attribute:
+
+    //make sure the book_ref is not repeating:
+    // finish generate book_ref:
+    while (exist.rows[0] == true) {
+      console.log("Oops, same book_ref has been generated");
+      book_ref = randomValueHex(6);
+      exist = await pool.query(
+        "SELECT book_ref FROM bookings where book_ref=$1",
+        [book_ref]
+      );
+    }
+
+    // now calculate the total amount:
+    var total_amount = 0;
+    for (i = 0; i < book_info.length; i++) {
+      total_amount += book_info[i].amount;
+    }
+    // insert the info to the bookings table:
     await pool.query(
-      "UPDATE flights SET seats_available = seats_available-1, seats_booked = seats_booked+1 WHERE flight_id=$1",
-      [st[0]]
+      "INSERT INTO bookings (book_ref, book_date, total_amount) VALUES ($1,now(),$2)",
+      [book_ref, total_amount]
     );
-    const departTime = await pool.query(
-      "SELECT scheduled_departure FROM flights WHERE flight_id=$1",
-      [st[0]]
-    );
-    const arrivalTime = await pool.query(
-      "SELECT scheduled_arrival FROM flights WHERE flight_id=$1",
-      [st[0]]
-    );
-    const ticketNO = newReserve.rows.ticket_no;
-    const flightID = st[0];
 
-    //insert new entry into boarding_passes:
+    // create a book_ref entry:
 
-    res.json(newReserve);
+    // const book_ref = await pool.query()
+
+    // const newReserve = await pool.query(
+    //   `INSERT INTO ticket (flight_id, movie, meal, name, checked_bag, amount_wotax, discount, phone, email) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
+    //   [
+    //     info[0],
+    //     info[1],
+    //     info[2],
+    //     info[3],
+    //     info[4],
+    //     info[5],
+    //     info[6],
+    //     info[7],
+    //     info[8],
+    //   ]
+    // );
+    // // update the seatnumber attribute:
+    // await pool.query(
+    //   "UPDATE flights SET seats_available = seats_available-1, seats_booked = seats_booked+1 WHERE flight_id=$1",
+    //   [st[0]]
+    // );
+    // const departTime = await pool.query(
+    //   "SELECT scheduled_departure FROM flights WHERE flight_id=$1",
+    //   [st[0]]
+    // );
+    // const arrivalTime = await pool.query(
+    //   "SELECT scheduled_arrival FROM flights WHERE flight_id=$1",
+    //   [st[0]]
+    // );
+    // const ticketNO = newReserve.rows.ticket_no;
+    // const flightID = st[0];
+
+    // //insert new entry into boarding_passes:
+
+    // res.json(newReserve);
   } catch (err) {
     console.log(err.message);
   }
