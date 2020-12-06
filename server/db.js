42340b03bede9e312fd8b65ae5b21ba7a46353e7
SongwenXue <60166898+OwenSit@users.noreply.github.com>
2020-12-05 22:02:37 -0600

    update

--- server/db.js
+++ server/db.js
@@ -1,16 +1,22 @@
 // CREATE TABLE todo(
 //   todo_id SERIAL PRIMARY KEY,
-//   description VARCHAR(255)  
+//   description VARCHAR(255)
 // );
 // only two attributes in the table: todo_id and description
-const Pool = require('pg').Pool;
+const Pool = require("pg").Pool;
 
 const pool = new Pool({
+<<<<<<< Updated upstream
   host: 'code.cs.uh.edu',
   user: 'cosc0213',
   password: '1787457XS',
+=======
+  host: "code.cs.uh.edu",
+  user: "cosc0213",
+  password: "1787457XS",
+>>>>>>> Stashed changes
   port: 5432,
-  database: 'COSC3380'
+  database: "COSC3380",
 });
 
 module.exports = pool;
