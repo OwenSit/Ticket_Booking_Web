777e676546996cb77df076513a6174ff0529b01b
SongwenXue <60166898+OwenSit@users.noreply.github.com>
2020-12-10 17:48:22 -0600

    updates

--- server/db.js
+++ server/db.js
@@ -1,16 +1,20 @@
 // CREATE TABLE todo(
 //   todo_id SERIAL PRIMARY KEY,
-//   description VARCHAR(255)  
+//   description VARCHAR(255)
 // );
 // only two attributes in the table: todo_id and description
-const Pool = require('pg').Pool;
+const data = require("./password.json");
+const Pool = require("pg").Pool;
+var username, password;
+username = data.name;
+password = data.password;
 
 const pool = new Pool({
-  host: 'code.cs.uh.edu',
-  user: 'cosc0213',
-  password: '1787457XS',
+  host: "code.cs.uh.edu",
+  user: username,
+  password: password,
   port: 5432,
-  database: 'COSC3380'
+  database: "COSC3380",
 });
 
 module.exports = pool;
